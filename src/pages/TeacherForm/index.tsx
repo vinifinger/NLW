0bb26c533a35439a3f902dc0cb8c8f17f3ebbe6b
Vinicius Marques Finger <vinicius.finger@pmweb.local>
2020-08-04 23:44:55 -0300

    NLW - Aula 1

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form">
+            <PageHeader title="Que incrível que você quer dar aulas." />
+        </div>
+    )
+}
+
+export default TeacherForm;
